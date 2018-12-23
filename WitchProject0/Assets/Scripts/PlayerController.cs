1fd948b630a90269f6636c118f2de69d82ae7e24
acekidprod <acekidprod@gmail.com>
2018-12-23 23:38:35 +0100

    Add empty project

---
+++ WitchProject0/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float walkSpeed = 5;
+    public float runSpeed = 10;
+
+    public float turnSmoothTime = 0.08f;
+    float turnSmoothVelocity;
+
+    Animator animator;
+    Transform cameraT;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+        cameraT = Camera.main.transform;
+	}
+
+	void Update () {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        float speed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+
+        transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
+
+        float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
+        animator.SetFloat("speedPercent", animationSpeedPercent);
+	}
+}
---
+++ WitchProject0/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public bool lockCursor;
+    public float cameraSensitivity = 1;
+
+    public Transform target; //player character
+    public float dstFromTarget = 2; //distance from player character
+    public Vector2 yawMinMax = new Vector2(-45, 45);
+
+    float yaw; //horizontal axis
+    float pitch = 45f; //vertical axis
+
+	void Start () {
+		if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+	}
+	
+    //LateUpdate = appelé après Update
+	void LateUpdate () {
+        float sideCameraValue = Input.GetAxisRaw("Mouse X");
+        float frontCameraValue = Input.GetAxisRaw("Mouse Y");
+        //à passer sur joystick droit
+
+        yaw += sideCameraValue * cameraSensitivity; 
+        yaw = Mathf.Clamp(yaw, yawMinMax.x, yawMinMax.y);
+        //force yaw à être entre yawmin et yawmax
+
+        if (frontCameraValue > 0 && yaw > 0)
+        {
+            yaw = yaw - frontCameraValue;
+        }
+        if (frontCameraValue > 0 && yaw < 0)
+        {
+            yaw = yaw + frontCameraValue;
+        }
+        if (frontCameraValue < 0 && yaw > 0)
+        {
+            yaw = yaw + frontCameraValue;
+        }
+        if (frontCameraValue < 0 && yaw < 0)
+        {
+            yaw = yaw - frontCameraValue;
+        }
+        //remettre la caméra au milieu
+
+        Vector3 targetRotation = new Vector3(pitch, yaw);
+        transform.eulerAngles = targetRotation;
+        //virage de caméra progressif
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+        //target = player character
+    }
+}
